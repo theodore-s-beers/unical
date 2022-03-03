1b3851d660d098d026be3b7ff1893b005be9acfb
Theo Beers <32523293+theodore-s-beers@users.noreply.github.com>
2022-03-03 18:55:53 +0100

    Keep adding tests

--- tests.ts
+++ tests.ts
@@ -2,39 +2,54 @@ import { assertEquals } from 'https://deno.land/std@0.127.0/testing/asserts.ts'
 import {
   leapGregorian,
   gregorianToJd,
-  jdToGregorian,
   leapJulian,
-  hebrewLeap
+  hebrewLeap,
+  islamicToJd,
+  jdToJulian,
+  jdToIslamic,
+  julianToJd
 } from './unical.ts'
 
-Deno.test('leap year', () => {
-  const leap: boolean = leapGregorian(2020)
+Deno.test('gregorian 2020 leap', () => {
+  const leap = leapGregorian(2020)
   assertEquals(leap, true)
 })
 
-Deno.test('non-leap year', () => {
-  const leap: boolean = leapGregorian(1900)
+Deno.test('gregorian 1900 non-leap', () => {
+  const leap = leapGregorian(1900)
   assertEquals(leap, false)
 })
 
-Deno.test('julian leap year', () => {
-  const leap: boolean = leapJulian(1900)
+Deno.test('julian 1900 leap', () => {
+  const leap = leapJulian(1900)
   assertEquals(leap, true)
 })
 
-Deno.test('hebrew leap year', () => {
+Deno.test('hebrew 4321 leap', () => {
   const leap = hebrewLeap(4321)
   assertEquals(leap, true)
 })
 
-Deno.test('amasya gregorian to jd', () => {
-  const day: number = gregorianToJd(1555, 6, 8)
-  assertEquals(day, 2289169.5)
+Deno.test('amasya gregorian to julian', () => {
+  const jd = gregorianToJd(1555, 6, 8)
+  const [year, month, day] = jdToJulian(jd)
+  assertEquals(year, 1555)
+  assertEquals(month, 5)
+  assertEquals(day, 29)
 })
 
-Deno.test('amasya jd to gregorian', () => {
-  const [year, month, day] = jdToGregorian(2289169.5)
-  assertEquals(year, 1555)
-  assertEquals(month, 6)
+Deno.test('amasya julian to islamic', () => {
+  const jd = julianToJd(1555, 5, 29)
+  const [year, month, day] = jdToIslamic(jd)
+  assertEquals(year, 962)
+  assertEquals(month, 7)
   assertEquals(day, 8)
+})
+
+Deno.test('suleymaniye islamic to julian', () => {
+  const jd = islamicToJd(957, 5, 26)
+  const [year, month, day] = jdToJulian(jd)
+  assertEquals(year, 1550)
+  assertEquals(month, 6)
+  assertEquals(day, 12)
 })

fb441d79de89926c0dc1a46341af88516458cfea
Theo Beers <32523293+theodore-s-beers@users.noreply.github.com>
2022-10-21 02:11:18 +0200

    Refactor tests

--- tests.ts
+++ tests.ts
@@ -1,55 +1,45 @@
 import { assertEquals } from 'https://deno.land/std@0.127.0/testing/asserts.ts'
 import {
-  leapGregorian,
   gregorianToJD,
-  leapJulian,
   hebrewLeap,
   islamicToJD,
-  jdToJulian,
   jdToIslamic,
-  julianToJD
+  jdToJulian,
+  julianToJD,
+  leapGregorian,
+  leapJulian
 } from './unical.ts'
 
 Deno.test('gregorian 2020 leap', () => {
-  const leap = leapGregorian(2020)
-  assertEquals(leap, true)
+  assertEquals(leapGregorian(2020), true)
 })
 
 Deno.test('gregorian 1900 non-leap', () => {
-  const leap = leapGregorian(1900)
-  assertEquals(leap, false)
+  assertEquals(leapGregorian(1900), false)
 })
 
 Deno.test('julian 1900 leap', () => {
-  const leap = leapJulian(1900)
-  assertEquals(leap, true)
+  assertEquals(leapJulian(1900), true)
 })
 
 Deno.test('hebrew 4321 leap', () => {
-  const leap = hebrewLeap(4321)
-  assertEquals(leap, true)
+  assertEquals(hebrewLeap(4321), true)
 })
 
 Deno.test('amasya gregorian to julian', () => {
   const jd = gregorianToJD(1555, 6, 8)
   const [year, month, day] = jdToJulian(jd)
-  assertEquals(year, 1555)
-  assertEquals(month, 5)
-  assertEquals(day, 29)
+  assertEquals([year, month, day], [1555, 5, 29])
 })
 
 Deno.test('amasya julian to islamic', () => {
   const jd = julianToJD(1555, 5, 29)
   const [year, month, day] = jdToIslamic(jd)
-  assertEquals(year, 962)
-  assertEquals(month, 7)
-  assertEquals(day, 8)
+  assertEquals([year, month, day], [962, 7, 8])
 })
 
 Deno.test('suleymaniye islamic to julian', () => {
   const jd = islamicToJD(957, 5, 26)
   const [year, month, day] = jdToJulian(jd)
-  assertEquals(year, 1550)
-  assertEquals(month, 6)
-  assertEquals(day, 12)
+  assertEquals([year, month, day], [1550, 6, 12])
 })
